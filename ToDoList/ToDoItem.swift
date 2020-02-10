e13eb5e4e83f579146067062d099f4ac5b949e9e
hfishstyx <hfishstyx@users.noreply.github.com>
2020-02-10 15:36:10 -0500

    Commit Message

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Harrison Fish on 2/10/20.
+//  Copyright © 2020 Harrison Fish. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Harrison Fish on 2/8/20.
+//  Copyright © 2020 Harrison Fish. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+
+    var toDoItem: ToDoItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if toDoItem == nil {
+            toDoItem = ToDoItem(name: "", date: Date(), notes: "")
+        }
+        
+        nameField.text = toDoItem.name
+        datePicker.date = toDoItem.date
+        noteView.text = toDoItem.notes
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text)
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode{
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+    }
+    
+}
+}
